4201829ad1ffd4deaecea76c080c476e6848b638
FMorales-afk <francisco_morales15@me.com>
2022-04-26 19:45:53 -0700

    Added couple more integration tests

--- test_integrations.py
+++ test_integrations.py
@@ -11,9 +11,13 @@ class TestCalculator(unittest.TestCase):
      self.assertEqual(calculator.multiply(3,7), 21)
      self.assertEqual(calculator.divide(100,2), 50)
 
-    def check_user_input(self):
-     self.assertEqual(calculator.choice('1'), '1')
-     self.assertEqual(calculator.choice('2'), '2')
+    def test_multiply_subtract(self):
+      self.assertEqual(calculator.multiply(3,7), 21)
+      self.assertEqual(calculator.subtract(10,5), 5)
    
+    def test_divide_add(self):
+     self.assertEqual(calculator.divide(100,2), 50)
+     self.assertEqual(calculator.add(4,7), 11)
+
 if __name__ == "__main__":
         unittest.main()
